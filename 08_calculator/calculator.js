3c63bce7fd2d080d98af2e6de9576e75b21e8ff2
M.Kristóf <drzero123456@gmail.com>
2022-03-27 20:36:57 +0200

    Latest exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return Number(a) - Number(b)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	total = 0;
+  arr.forEach(number => {
+    total += number
+  })
+  return total;
 };
 
 const multiply = function() {
+  if (arguments.length === 0) {
+    return 0;
+  } else {
+    total = 1;
+    for (let i = 0;i < arguments[0].length;i++) {
+      total *= Number(Array.from(arguments[0])[i])
+    }
+    return total
+  }
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	if (val < 2) {
+    return 1;
+  } else {
+    total = 1
+    while (val > 1) {
+      total *= val;
+      val--;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let alpha_string = "";
+  for (let i = 0;i < string.length;i++) {
+      if (string[i].toLowerCase() != string[i].toUpperCase()) {
+          alpha_string+= String(string[i])
+      }
+  }
+  console.log(alpha_string)
 
+  reversed = alpha_string.split("").reverse().join("");
+  return alpha_string.toLowerCase() === reversed.toLowerCase();
 };
 
 // Do not edit below this line
