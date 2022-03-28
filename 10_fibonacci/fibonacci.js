81bf11454dd8fe1211fb93c28718cda2ea0edd5d
M.Kristóf <drzero123456@gmail.com>
2022-03-28 19:31:59 +0200

    Fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = (num = 1) => {
+    if (num < 0) {
+        return "OOPS"
+    }else {
+        const series = [1, 1];
+        for (let i = 2; i < num; i++) {
+           const a = series[i - 1];
+           const b = series[i - 2];
+           series.push(a + b);
+        };
+        return series[num - 1];
+    }
 
-};
+ };
+ 
 
 // Do not edit below this line
 module.exports = fibonacci;
